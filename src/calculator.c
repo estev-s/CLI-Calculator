057977327d84daf2235bad56d9f17897181f725a
Estevão Salles <mail.estevao.s@gmail.com>
2026-02-13 23:20:58 -0300

    Improvements made and possible fixes found:
    Improvements:
    	- safer initialization of variables.
    	- better sscanf char format assignment (" %c").
    
    Possible fixes to existing bugs:
    	- Need to add cleaner for input stream buffer.
    
    Signed-off-by: Estevão Salles <mail.estevao.s@gmail.com>

--- src/calculator.c
+++ src/calculator.c
@@ -34,16 +34,18 @@ void sqroot(void);
 int main(void)
 {
 	
-	char input[5];
-	char confirmation;
+	char input[2] = {0};
+	char confirmation = '\0';
+	
 	do {
 		printf("Hi! I'm the calculator!\nDo you want to calculate? (Y/N) ");
 		fgets(input, sizeof(input), stdin);
 		sscanf(input, " %c", &confirmation); // " %c" more prone to user typos.
 		
 		if (confirmation != 'Y' && confirmation != 'N' && confirmation != 'y' && confirmation != 'n')
-			
+
 			printf("Invalid answer, try again...\n");
+			
 
 	} while (confirmation != 'Y' && confirmation != 'N' && confirmation != 'y' && confirmation != 'n');
 
@@ -53,22 +55,23 @@ int main(void)
     
 	while (confirmation == 'Y' || confirmation == 'y') {
 
+		char number[2] = {0};
+		int operation = 0;
+		
 		do {
 
-			char number[3];
-			int operation;	
 			printf("Operations:\n1. + \n2. - \n3. * \n4. / \n5. ^\n6. Sqroot\n \n");
 			printf("Choose your operation number: ");
 			fgets(number, sizeof(number), stdin);
 			sscanf(number,"%i", &operation);
 			
-			if (operation != 1 && operation != 2 && operation != 3 && operation != 4 && operation != 5
-				&& operation != 6)
+			if (operation != 1 && operation != 2 && operation != 3 && operation != 4 
+					&& operation != 5 && operation != 6)
 
 				printf("Invalid answer, try again...\n");	
 
-		} while (operation != 1 && operation != 2 && operation != 3 && operation != 4 && operation != 5
-				&& operation != 6);
+		} while (operation != 1 && operation != 2 && operation != 3 && operation != 4 
+				&& operation != 5 && operation != 6);
 
 		switch (operation) {
 
@@ -103,11 +106,13 @@ int main(void)
 			fgets(input, sizeof(input), stdin);
 			sscanf(input, " %c", &confirmation);
 
-			if (confirmation != 'Y' && confirmation != 'N' && confirmation != 'y' && confirmation != 'n')
+			if (confirmation != 'Y' && confirmation != 'N' && confirmation != 'y' 
+					&& confirmation != 'n')
 				
 				printf("Invalid answer, try again...\n");
 			
-		} while (confirmation != 'Y' && confirmation != 'N' && confirmation != 'y' && confirmation != 'n');
+		} while (confirmation != 'Y' && confirmation != 'N' && confirmation != 'y' 
+				&& confirmation != 'n');
 
 		
 		if (confirmation == 'N' || confirmation == 'n') {
@@ -133,8 +138,8 @@ int main(void)
 void sum(void)
 {
 
-	char num1[100], num2[100];
-	double x, y, z;
+	char num1[100] = {0}, num2[100] = {0};
+	double x = 0, y = 0, z = x + y;
 	printf("First number: ");
 	fgets(num1, sizeof(num1), stdin);
 	sscanf(num1, "%lf", &x);
@@ -143,7 +148,6 @@ void sum(void)
 	fgets(num2, sizeof(num2), stdin);
 	sscanf(num2, "%lf", &y);
 	
-	z = x + y;
 	printf("Result: "), printf("%.2f", z);
 	
 	
